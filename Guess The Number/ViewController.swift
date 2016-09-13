de2e01f2a8a6393103b1e88927c518a67de1ff39
Mike Mallow <mikemallow@mikes-macbook-pro-2.local>
2016-09-13 00:04:49 -0400

    Initial Commit

---
+++ Guess The Number/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Guess The Number
+//
+//  Created by Mike Mallow on 9/13/16.
+//  Copyright © 2016 Mike Mallow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
